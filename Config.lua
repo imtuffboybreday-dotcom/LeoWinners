3eb14346a8a218e97e6d234b3c803650dedcbca2
imtuffboybreday-dotcom <imtuffboybreday@gmail.com>
2026-09-18 21:58:53 +0700

    Add files via upload

---
+++ Config.lua
@@ -0,0 +1,19 @@
+--[[
+    Do not edit unless you know what you are doing.
+]]
+
+return {
+    Version = "3.0.0",
+    KickMessage = "Ur A Bum Skidder Lol",
+    ExpectedDomain = "raw.githubusercontent.com",
+    CheckInterval = 2.5, --
+    Debug = false,
+
+    -- Integrity markers
+    MarkerA = "ANTI_SKID_STAGE_ONE",
+    MarkerB = "ANTI_SKID_STAGE_TWO",
+    MarkerC = "ANTI_SKID_STAGE_THREE",
+
+    -- Optional
+    OwnerUserId = 0
+}   
